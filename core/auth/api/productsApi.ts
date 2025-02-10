1a083894c05bc6e58ed439ae393c727e0d4664ef
kevinmartinezc <ceronk003@gmail.com>
2025-02-10 15:18:42 -0600

    feat: add evn variables

--- core/auth/api/productsApi.ts
+++ core/auth/api/productsApi.ts
@@ -1,8 +1,18 @@
 import axios from "axios";
+import { Platform } from "react-native";
 //Todo colectar mediante envs vars, android e IOSs
 
+const STAGE = process.env.EXPO_PUBLIC_STAGE || "dev";
+export const API_URL =
+  STAGE === "prod"
+    ? process.env.EXPO_PUBLIC_API_URL
+    : Platform.OS === "ios"
+    ? process.env.EXPO_PUBLIC_API_IOS
+    : process.env.EXPO_PUBLIC_API_ANDROID;
+
+console.log({ STAGE, [Platform.OS]: API_URL });
 const productsApi = axios.create({
-  baseURL: "localhost:3000/api",
+  baseURL: API_URL,
 });
 
 //TODO interceptores
