4ca38bce4042f4ea7eb13e0f81d781caa9cb4a1b
kevinmartinezc <ceronk003@gmail.com>
2025-02-11 20:03:20 -0600

    feat: add infinity scroll and pull to refresh

--- presentation/products/components/ProductList.tsx
+++ presentation/products/components/ProductList.tsx
@@ -1,8 +1,8 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Product } from "@/core/products/interfaces/product.interface";
-import { FlatList } from "react-native-gesture-handler";
 import { ProductCard } from "./ProductCard";
+import { FlatList, RefreshControl } from "react-native";
+import { queryClient } from "@/core/utils/queryClient";
 
 interface Props {
   products: Product[];
@@ -10,16 +10,34 @@ interface Props {
 }
 
 const ProductList = ({ products, loadNextPage }: Props) => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onPullToRefresh = async () => {
+    setIsRefreshing(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 200));
+    queryClient.invalidateQueries({
+      queryKey: ["products", "infinite"],
+    });
+    setIsRefreshing(false);
+  };
+
   return (
     <FlatList
       data={products}
       numColumns={NUM_OF_COLUMNS}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <ProductCard product={item} />}
+      onEndReached={loadNextPage}
+      onEndReachedThreshold={0.8}
+      showsVerticalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />
+      }
     />
   );
 };
 
 export default ProductList;
 
-const NUM_OF_COLUMNS = 2
+const NUM_OF_COLUMNS = 2;
